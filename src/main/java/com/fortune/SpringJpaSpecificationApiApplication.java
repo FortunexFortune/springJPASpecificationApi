5f096215961ee9351a46f37a9af1b421b123fdae
fortunexfortune <osadolor.fortune@yahoo.it>
2020-02-22 00:19:39 +0000

    my first commit

---
+++ src/main/java/com/fortune/SpringJpaSpecificationApiApplication.java
@@ -0,0 +1,13 @@
+package com.fortune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaSpecificationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaSpecificationApiApplication.class, args);
+	}
+
+}
---
+++ utils/account.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "Account" (
+  "account_id" int PRIMARY KEY,
+  "name" varchar,
+  "email" varchar,
+  "password" varchar,
+  "date_created" timestamptz
+);
